087409231e3c0938fda4e13bd813b9e8c783cf57
Melumi <roy.zixiao.huang@gmail.com>
2021-03-19 23:05:18 -0400

    Update Fegg.py
    Make stuff look cool using tags

--- Fegg.py
+++ Fegg.py
@@ -4,17 +4,18 @@ import discord, logging, asyncio
 
 #-------------------------------variables-------------------------------#
 client = discord.Client()
-TOKEN = 'ODIyNDc0NzIxNTI1NjI4OTc5.YFSzRg.NE2o2SRi4Dr6XwBWDCuwQ0Ttv1M'
+TOKEN = 'ODIyNDc0NzIxNTI1NjI4OTc5.YFSzRg.NE2o2SRi4Dr6XwBWDCuwQ0Ttv1M' #Bot token
+#Lucky numbers of the members in the Arena, as a dictionary
 luckies = {822474721525628979: 1, 259716396198395904: 2, 543857545278783520: 3, 400514653957914635: 4, 246080207704817664: 5, 567819726013726722: 6, 332711880831270912: 7, 320559692269223938: 8, 194310041900154880: 9, 548617575282769922: 10, 640714673045504020: 11, 175824478423482368: 13, 294736827946893313: 16, 748751242003611739: 18, 731368690364186634: 19}
-fighting = False
-p1, p2 = None, None
-PLAYERHP = 100
+fighting = False #for the Arena game
+#p1, p2 = None, None #defining players, will be changed to Fighter class on init
+PLAYERHP = 100 #Starting health for both players
 #-----------------------------------------------------------------------#
 
 #Logging errors
 logging.basicConfig(level=logging.ERROR)
 logging.basicConfig(level=logging.CRITICAL)
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.WARNING) #Errors and stuff show in the terminal
 
 #Launch text in terminal
 @client.event
@@ -29,17 +30,17 @@ async def on_ready():
 #Reading messages
 @client.event
 async def on_message(message):
-    global fighting #takes fighting status variable
     if message.author == client.user: #so that the bot doesn't message itself
         return
     
+    global fighting #takes fighting status variable
     if fighting == True: #If people are fighting then we call fight
         await asyncio.gather(fight(message))
-
+    
     if message.content.startswith("!fight"): #begin fight if no one is fighting
         if fighting == False:
             await asyncio.gather(startfight(message))
-        else:
+        else: 
             await message.channel.send("There is already a fight going on between " + str(p1.tag) + " and " + str(p2.tag) + " and the maker of this bot did not anticipate that he needed to add code for more than one battle at a time. Please contact Melumi#5395 or ask the current fighters to wrap up or quit their game. Thank you.")
         
     elif ('kill me' or ('i' and 'die')) in message.content.lower(): #suicide prevention
@@ -49,17 +50,15 @@ async def on_message(message):
     elif 'kill' in message.content.lower(): #knife emoji
         await message.channel.send(":knife:")
 
-    elif '...' in message.content.lower(): #blank message
-        await message.channel.send("** **")
-
     elif message.content.lower() == '!sweat': #big sweat
         await message.channel.send("https://cdn.discordapp.com/attachments/822493563619246131/822498710873178133/unknown.png")
     
-    elif ('fegg' in message.content.lower() and message.author.id == 640714673045504020): #my text
-        await message.channel.send("Greetings, most noble creator <:fleggbeg:800558355714015263>")
-    
     elif 'fegg' in message.content.lower(): #greetings
-        await message.channel.send('Hello!')
+        embedVar = discord.Embed(title="Hello", description="My name is Fegg.", color=0x00ff00)
+        embedVar.add_field(name="Your name is not Fegg.", value="hi", inline=False)
+        embedVar.add_field(name="Your name is "+ str(message.author), value="hi again", inline=False)
+        embedVar.set_thumbnail(url=(message.author.avatar_url))
+        await message.channel.send(embed=embedVar)
 
 async def startfight(message): #called when a fight starts
     global fighting
@@ -77,10 +76,17 @@ async def startfight(message): #called when a fight starts
         else:
             fighting = True
             try:
+                embedVar = discord.Embed(title=("**" + p1.tag.name + "** challenges **" + p2.tag.name + "** to a battle!"), description="The first player to lose all their health loses.", color=0x00ff00)
+                embedVar.add_field(name=("**" + p1.tag.name + "**'s lucky number is " + str(luckies[p1.id]) + " and **" + p2.tag.name + "**'s is " + str(luckies[p2.id])), 
+                                    value=("Type `roll` to attack and `!quit` to stop fighting."), inline=False)
+                embedVar.set_thumbnail(url=(message.author.avatar_url))
+                await message.channel.send(embed=embedVar)
+                """
                 await message.channel.send(p1.name + " challenges " + p2.name + " to a battle!") #if no errors, the battle text comes
                 await message.channel.send("The first person to lose all their health loses.")
                 await message.channel.send("Type `roll` to attack and `!quit` to stop fighting.")
                 await message.channel.send(p1.name + "'s lucky number is " + str(luckies[p1.id]) + " and " + p2.name + "'s is " + str(luckies[p2.id]))
+                """
             except KeyError:
                 try:
                     luckies[p1.id] #checking to see both players' lucky numbers are registered
@@ -98,12 +104,23 @@ async def fight(message):
     global p2
     global fighting
     if (message.content.lower() == "!quit" and (message.author.id == p1.id or message.author.id == p2.id)): #to abort the match
+        embedVar = discord.Embed(title="The match between " + p1.tag.name + " and " + p2.tag.name + " has been aborted.", description=(p1.name + " hp: " + str(p1.hp) + "\n" + p2.name + " hp: " + str(p2.hp)), color=0x00ff00)
+        await message.channel.send(embed=embedVar) #match over and hp text
+
+        embedVar = discord.Embed(title=(p1.tag.name + "'s stats:"), description=p1.stats(), color=0x00ff00)
+        embedVar.set_thumbnail(url=(p1.tag.avatar_url)) #stats for player 1
+        await message.channel.send(embed=embedVar)
+
+        embedVar = discord.Embed(title=(p2.tag.name + "'s stats:"), description=p2.stats(), color=0x00ff00)
+        embedVar.set_thumbnail(url=(p2.tag.avatar_url)) #stats for player 2
+        await message.channel.send(embed=embedVar)
+        """
         await message.channel.send("The match has been aborted.")
         await message.channel.send("The score is\n" + p1.name + " hp: " + str(p1.hp) + "\n" + p2.name + " hp: " + str(p2.hp))
         await message.channel.send("Stats:")
-        await message.channel.send(p1.stats())
+        await message.channel.send(p1.stats()) #Sends HP and stats
         await message.channel.send(p2.stats())
-        print("hello")
+        """
         fighting = False
 
 class Fighter:
@@ -119,7 +136,8 @@ class Fighter:
     def reset(self, tag):
         self.tag = tag #discord tag
         self.id = self.tag.id
-        self.name = "<@" +str(self.id) + ">" #<@numbers>
+        #self.name = "<@" +str(self.id) + ">" #<@numbers>
+        self.name = self.tag.name
         self.hp = PLAYERHP
         self.ones = 0
         self.twenties = 0
@@ -136,3 +154,5 @@ client.run(TOKEN)
 
 #wait:
 #await asyncio.sleep(10)
+#reply:
+#await ctx.reply('Hello!', mention_author=False)
