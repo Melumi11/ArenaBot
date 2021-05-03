9d23ce19ead895b3b3f8faf45dd9877200844d90
Thomas Ricci <git@thomasricci.dev>
2021-05-02 21:07:26 -0400

    removed stopping command

--- bot.py
+++ bot.py
@@ -295,24 +295,6 @@ async def roll(ctx, lucky):
 
 # -------------------------------Launch bot------------------------------#
 
-def isData():
-    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
-
-
-old_settings = termios.tcgetattr(sys.stdin)
-try:
-    tty.setcbreak(sys.stdin.fileno())
-    i = 0
-    while 1:
-        i += 1
-        if isData():
-            c = sys.stdin.read(1)
-            if c == 'stop':
-                break
-
-finally:
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-
 with open('token.txt') as f:
     TOKEN = f.readline()
 client.run(TOKEN)
